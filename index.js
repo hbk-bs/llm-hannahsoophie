4e9883cd657c808b3ef5cdf275ab8af1d9a471e3
hannahsoophie <h.flintermann@hbk-bs.de>
2024-06-03 14:27:51 +0200

    all

--- index.js
+++ index.js
@@ -24,47 +24,195 @@
  */
 
 // import the wrapper class
-import { LLM } from './llm.js';
+import { LLM } from "./llm.js";
 
 // create an instance of the class
 // you need to insert the run url for your val.town openai api
 // @ff6347 will instruct you on how to get this
 
 const llm = new LLM({
-  host: '<insert run url for your val.town openai api here>',
+  host: "https://hannahsoophie-openai_api.web.val.run/",
+});
+const information = [];
+
+const infos = {
+  extraButtons: "",
+  boomButton: ""
+}
+// get the button#run element from the index.html
+const loadingSpinner = document.getElementById("loading-spinner");
+const extraButton1 = document.getElementById("extra-button-1");
+const extraButton2 = document.getElementById("extra-button-2");
+const extraButton3 = document.getElementById("extra-button-3");
+const extraButton4 = document.getElementById("extra-button-4");
+const startButton = document.getElementById("start");
+const runModelButton = document.getElementById("runModel");
+const timedTextDiv = document.getElementById("timed-text");
+const extraButtonsContainer = document.getElementById('extra-buttons-container');
+const finishedButton = document.getElementById("finished");
+const chooseEmotion = document.getElementById("extra-buttons-text")
+const wellDoneText = document.getElementById("well-done");
+
+
+wellDoneText.classList.add("hidden");
+
+// timedTextDiv.addEventListener("", () => {
+//   timedTextDiv.classList.remove("hidden")
+// })
+// timedTextDiv.addEventListener("", () => {
+//   timedTextDiv.classList.add("hidden")
+// })
+
+startButton.addEventListener("click", () => {
+  extraButtonsContainer.classList.remove('hidden');
+  extraButton1.classList.remove("hidden");
+  extraButton2.classList.remove("hidden");
+  extraButton3.classList.remove("hidden");
+  extraButton4.classList.remove("hidden");
+  startButton.classList.add("hidden");
+  timedTextDiv.classList.add("hidden"); // Hide the "Choose your emotion" section
+  chooseEmotion.classList.remove("hidden");
 });
 
-// get the button#run element from the index.html
-const chatButton = document.getElementById('run');
+extraButton1.addEventListener("click", () => {
+  information.push("enthousiastic");
+  extraButton1.classList.add("hidden");
+  extraButton2.classList.add("hidden");
+  extraButton3.classList.add("hidden");
+  extraButton4.classList.add("hidden");
+  runModelButton.classList.remove("hidden");
+  timedTextDiv.classList.add("hidden"); // Hide the "Choose your emotion" section
+  chooseEmotion.classList.add("hidden");
+});
+
+extraButton2.addEventListener("click", () => {
+  information.push("sad");
+  extraButton1.classList.add("hidden");
+  extraButton2.classList.add("hidden");
+  extraButton3.classList.add("hidden");
+  extraButton4.classList.add("hidden");
+  runModelButton.classList.remove("hidden");
+  timedTextDiv.classList.add("hidden"); // Hide the "Choose your emotion" section
+  chooseEmotion.classList.add("hidden");
+});
+
+extraButton3.addEventListener("click", () => {
+  information.push("anxious");
+  extraButton1.classList.add("hidden");
+  extraButton2.classList.add("hidden");
+  extraButton3.classList.add("hidden");
+  extraButton4.classList.add("hidden");
+  runModelButton.classList.remove("hidden");
+  timedTextDiv.classList.add("hidden"); // Hide the "Choose your emotion" section
+  chooseEmotion.classList.add("hidden");
+});
+extraButton4.addEventListener("click", () => {
+  information.push("peaceful");
+  extraButton1.classList.add("hidden");
+  extraButton2.classList.add("hidden");
+  extraButton3.classList.add("hidden");
+  extraButton4.classList.add("hidden");
+  runModelButton.classList.remove("hidden");
+  timedTextDiv.classList.add("hidden"); // Hide the "Choose your emotion" section
+  chooseEmotion.classList.add("hidden");
+});
+function handleButtonClick(feeling) {
+  information.push(feeling);
+  extraButton1.classList.add("hidden");
+  extraButton2.classList.add("hidden");
+  extraButton3.classList.add("hidden");
+  extraButton4.classList.add("hidden");
+  runModelButton.classList.remove("hidden");
+  timedTextDiv.classList.add("hidden"); // Hide the "Choose your emotion" section
+}
+
+
+
+const textSections = [
+  "we live in a society...",
+  "obsessed with making sense...",
+  "optimizing, glamourzing, bettering the self...",
+  "but what if, just for a minute...",
+  "we would throw all that aside...",
+  "and start playing again...",
+  "we invite you...",
+  "to complete one of our tasks daily...",
+  "to start doing things again...",
+  "without the desire for a certain outcome.",
+];
+let currentSectionIndex = 0;
+
+function displayNextTextSection() {
+  if (currentSectionIndex < textSections.length) {
+    timedTextDiv.innerText = textSections[currentSectionIndex];
+    currentSectionIndex++;
+    setTimeout(displayNextTextSection, 700); // Change text every 3 seconds
+  } else {
+    // Show the button after all text sections have been displayed
+    timedTextDiv.classList.add("hidden")
+    startButton.classList.remove("hidden");
+  }
+}
+
+// Start displaying the text sections
+displayNextTextSection();
 
 // add a click event listener to the button that runs the async function
-chatButton.addEventListener('click', async () => {
+runModelButton.addEventListener("click", async () => {
   // some options for the chat
-  const format = 'json'; // we want json output
+  console.log("this is your information you sleected from the user", information[0]); // ["sad" | "happy"]
+ // ["sad" | "happy"]
+
+  
+  const format = "json"; // we want json output
   // we set the seed so we get always the same output
   // we set the temperature which controls the creativity of the model
   const options = {
-    seed: 42,
-    temperature: 0.5,
+    // seed: 42,
+    temperature: 1.5,
   };
   // the messages that we want to send to the model
   // allowed are 'system', 'assistant' and 'user' role for the messages
   const messages = [
     {
-      role: 'system',
-      content:
-        'You are a helpful assistant. Always repond in JSON and only JSON',
+      role: "system",
+      content: ` Give me a random task to do, that can be finished in 1-5 Minutes. The task has to be set in the close environment. The user is feeling ${information[0]} today, so choose the task according to his mood (if he/she is anxious choose a claming task for him/her etc.). Make it very random, so that you would normally not think about doing a task like this. Make it slightly playful and creative, as if a child came up with the idea. Make it still mindful so that the user feels good about completing the task. Make it realistic, so that the user is able to successfully complete the task. Just give out the task with no context or extra information, like saying that you are about to give the user a task. You can use the examples as well. Make up a differnt task each time. Include a fitting emoji.
+      
+      
+      **Examples:** <Write a short poem about the last noise you heard. . Call the first person in your contacts. Hug a tree or a living being. Use household items to create a fun obstacle course in your living room.
+      Stand in an open space and spin around until you get dizzy, then lie down and watch the world spin.
+      Spend time walking backwards around your house or yard just to see how it feels.
+      Use tiny cups and plates to have a tea party with actual snacks.
+      Try walking around with a book balanced on your head to see how long you can keep it there.
+      Spend a few minutes acting like your favorite animal.> `,
     },
-    { role: 'user', content: 'What is the capital of the United States?' },
+    {
+      role: "user",
+      content:
+        "Give me a task to do every time I ask. Each time I ask, give me a different task",
+    },
   ];
 
   try {
     // now we make the call to the api.
     // we wrap it in a try catch block to catch any errors
-    const response = await llm.chat({ format, options, messages });
+    chooseEmotion.classList.add("hidden");
+    loadingSpinner.classList.remove("hidden"); // Show loading spinner
+    const response = await llm.chat({ options, messages });
     console.log(response);
+    const outputDiv = document.getElementById("output");
+    outputDiv.innerText = response.completion.choices[0].message.content;
+    finishedButton.classList.remove("hidden");
   } catch (error) {
     // we had an error lets handle it
     console.error(error);
+  }finally {
+      loadingSpinner.classList.add("hidden"); // Hide loading spinner
+ 
   }
+});
+finishedButton.addEventListener("click", () => {
+  finishedButton.classList.add("hidden");
+  startButton.classList.add("hidden")
+  wellDoneText.classList.remove("hidden");
 });
